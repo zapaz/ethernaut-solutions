4840a4877932bcf89bce3aae01d7176960ba1f6a
zapaz.eth <alain@kredeum.com>
2022-10-18 18:51:04 +0200

    all instances code

--- instances/04_telephone.sol
+++ instances/04_telephone.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.6.0;
 
 contract Telephone {
-    address public owner;
 
-    constructor() public {
-        owner = msg.sender;
-    }
+  address public owner;
 
-    function changeOwner(address _owner) public {
-        if (tx.origin != msg.sender) {
-            owner = _owner;
-        }
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+      owner = _owner;
     }
+  }
 }
--- instances/06_delegation.sol
+++ instances/06_delegation.sol
@@ -2,30 +2,32 @@
 pragma solidity ^0.6.0;
 
 contract Delegate {
-    address public owner;
 
-    constructor(address _owner) public {
-        owner = _owner;
-    }
+  address public owner;
 
-    function pwn() public {
-        owner = msg.sender;
-    }
+  constructor(address _owner) public {
+    owner = _owner;
+  }
+
+  function pwn() public {
+    owner = msg.sender;
+  }
 }
 
 contract Delegation {
-    address public owner;
-    Delegate delegate;
 
-    constructor(address _delegateAddress) public {
-        delegate = Delegate(_delegateAddress);
-        owner = msg.sender;
-    }
+  address public owner;
+  Delegate delegate;
 
-    fallback() external {
-        (bool result, ) = address(delegate).delegatecall(msg.data);
-        if (result) {
-            this;
-        }
+  constructor(address _delegateAddress) public {
+    delegate = Delegate(_delegateAddress);
+    owner = msg.sender;
+  }
+
+  fallback() external {
+    (bool result,) = address(delegate).delegatecall(msg.data);
+    if (result) {
+      this;
     }
+  }
 }
